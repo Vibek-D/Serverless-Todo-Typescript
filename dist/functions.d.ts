d425e25b536a638c9487cbbcaebd13410c2f6af9
Vibek-D <vikiisbck@gmail.com>
2021-07-04 13:30:52 +0530

    serverless webpack plugin removed using serverless plugin uninstall --name pluginName

--- dist/functions.d.ts
+++
@@ -1,7 +0,0 @@
-import { APIGatewayProxyEvent, APIGatewayProxyResult, Context } from 'aws-lambda';
-export declare type LambdaResponse = {
-    statusCode: number;
-    headers: {};
-    body: string;
-};
-export declare function ping(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult>;
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,43 +1,36 @@
-// module.exports = {
-//     target: 'node',
-//     mode: 'production',
-//     // entry: 'app.js',
-//     // output: {
-//     //     filename: './dist/app.bundle.js'
-//     // }
-// };
-
 const path = require('path');
 const slsw = require('serverless-webpack');
+import { Configuration } from 'webpack';
 import { resolve } from 'path';
 
-module.exports = {
-//   mode: process.env.NODE_ENV === 'dev' ? 'development' : 'production',
-mode: process.env.NODE_ENV === 'dev' ? 'development' : 'production',
-//   entry: slsw.lib.entries,
-  entry: { functions: './src/functions.ts'},
-  output: {
-    filename: '[name].js',
-    libraryTarget: 'commonjs2',
-    path: resolve(__dirname, 'build'),
-  },
-  resolve: {
-    extensions: ['.js', '.ts'],
-  },
-  target: 'node',
-  module: {
-    rules: [
-      {
-        test: /\.(tsx?)$/,
-        loader: 'ts-loader',
-        exclude: [
-          [
-            path.resolve(__dirname, 'node_modules'),
-            path.resolve(__dirname, '.serverless'),
-            path.resolve(__dirname, '.webpack'),
+const config: Configuration = {
+  // mode: process.env.NODE_ENV === 'dev' ? 'development' : 'production',
+  // entry: slsw.lib.entries,
+    entry: { functions: './src/functions.ts'},
+    output: {
+      filename: '[name].js',
+      libraryTarget: 'commonjs2',
+      path: resolve(__dirname, 'build'),
+    },
+    resolve: {
+      extensions: ['.js', '.ts'],
+    },
+    target: 'node',
+    module: {
+      rules: [
+        {
+          test: /\.(tsx?)$/,
+          loader: 'ts-loader',
+          exclude: [
+            [
+              path.resolve(__dirname, 'node_modules'),
+              path.resolve(__dirname, '.serverless'),
+              path.resolve(__dirname, '.webpack'),
+            ],
           ],
-        ],
-      },
-    ],
-  },
+        },
+      ],
+    },
 };
+
+export default config;
