2c131e2089d0149bf246ad20c43e60a870d9c1db
Vibek-D <vikiisbck@gmail.com>
2021-07-08 11:56:21 +0530

    build files changed

--- webpack.config.ts
+++ webpack.config.ts
@@ -6,9 +6,12 @@ import { resolve } from 'path';
 const config: Configuration = {
   // mode: process.env.NODE_ENV === 'dev' ? 'development' : 'production',
   // entry: slsw.lib.entries,
-    entry: { dataPost: './src/dataPost.ts', dataGet: './src/dataGet.ts', dataDelete: './src/dataDelete.ts'},
+    entry: { 
+      dataPost: './src/dataPost.ts',
+      dataGet: './src/dataGet.ts',
+      dataDelete: './src/dataDelete.ts'},
     output: {
-      filename: '[name].js',
+      filename: '[name].build.js',
       libraryTarget: 'commonjs2',
       path: resolve(__dirname, 'build'),
     },
